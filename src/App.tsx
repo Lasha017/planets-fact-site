312c4ae6fd763fabc0335f3c0e02423354c9aaa8
Lasha Gogolashvili <lashagogolashvili17@gmail.com>
2024-04-04 20:56:49 +0400

    app-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import Header from "./components/header/Header";
-import Planet from "./pages/landing/planet/Planet";
+import { useEffect } from "react";
+import Mars from "./pages/mars/Mars";
+import Mercury from "./pages/mercury/Mercury";
+import Venus from "./pages/venus/Venus";
+import ReactDOM from "react-dom";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const url = window.location.pathname;
+    let Component;
+
+    switch (url) {
+      case "/mars":
+        Component = Mars;
+        break;
+      case "/mercury":
+        Component = Mercury;
+        break;
+      case "/venus":
+        Component = Venus;
+        break;
+      default:
+        Component = Mars;
+    }
+
+    const element = <Component />;
+    ReactDOM.render(element, document.getElementById("root"));
+  }, []);
+
   return (
-    <>
-      <Header />
-      <Planet
-        name={""}
-        src={undefined}
-        text={""}
-        firstBtn={""}
-        secondBtn={""}
-        thirdBtn={""}
-      />
-    </>
+    <div className="App">
+      {/*... */}
+    </div>
   );
-}
+};
 
 export default App;
